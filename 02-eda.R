f32bdd377c35e0313176cb9f96b2c11b869fa299
azyansafriudin <22b6038@ubd.edu.bn>
2023-09-21 15:34:46 +0800

    Update task 2

--- 02-eda.R
+++ 02-eda.R
@@ -1,71 +1,51 @@
+# Task 2
+
+library(tibble)
+library(dplyr)
 library(tidyverse)
 
-#Task 2
+# Categorize the collatz_df tibble by sequence length 
+sorted_collatz_df <- collatz_df %>%
+  mutate(seq_len = lengths(sequence)) %>%
+  arrange(desc(seq_len))
 
-#Create a function to generate Collatz Sequences:
-generate_collatz_sequence <- function(initial_int) {
-  sequence <- c(initial_int)
-  
-  while (initial_int != 1) {
-    if (initial_int %% 2 == 0) {
-      initial_int <- initial_int / 2
-    } else {
-      initial_int <- 3 * initial_int + 1
-    }
-    sequence <- c(sequence, initial_int)
-  }
-  
-  return(sequence)
-}
+#Q1
+# The top initial integers
+initial_int <- 1:10000
 
-#Generate Collatz sequences
-
-# Construct the range of integers
-initial_integers <- 1:10000
-
-# Generate Collatz sequences for each initial integer and keep them in a data frame
-collatz_df <- tibble(
-  initial_int = initial_integers,
-  sequence = map(initial_integers, generate_collatz_sequence)
-)
-
-#Q1-------------
-# Determine the lengths of the sequences
-collatz_df <- collatz_df %>%
-  mutate(sequence_length = map_dbl(sequence, ~length(.))) %>%
-  arrange(desc(sequence_length))
-
-# top 10 starting integers
-top10longest <- collatz_df %>%
+top10longest <- sorted_collatz_df %>%
+  select(initial_int, seq_len) %>%
   head(10)
 
-#Q2-----------------------
-# Determine the max value of each sequence and find the initial integer with the highest max
+# Printing Q1
+print(top10longest)
+
+#Q2 
+# Determine the initial integer with the highest max value
 max_val_int <- collatz_df %>%
-  mutate(max_value = map_dbl(sequence, ~max(.))) %>%
-  filter(max_value == max(max_value)) %>%
-  select(initial_int)
+  mutate(max_value = sapply(sequence, max)) %>%
+  filter(max_value == max(max_value))  %>%
+  select(initial_int, max_value)
 
-#Q3----------
-# Create a new variable to group initial integers as even or odd
+# Printing Q2
+print(max_val_int)
+
+
+#Q3
+# Construct a new column to group even and odd initial integers
 collatz_df <- collatz_df %>%
-  mutate(is_even = initial_int %% 2 == 0)
+  mutate(even_odd = ifelse(initial_int %% 2 == 0, "even", "odd"))
 
-# Determine the average length and standard deviation for even and odd integers
+# Determine the avg length and standard deviation 
 even_odd_stats <- collatz_df %>%
-  group_by(is_even) %>%
+  group_by(even_odd) %>%
   summarize(
-    avg_len = mean(sequence_length),
-    sd_len = sd(sequence_length)
+    avg_len = mean(lengths(sequence)),
+    sd_len = sd(lengths(sequence))
   )
 
-# Generating the outcome
-even_odd_avg_len <- even_odd_stats %>%
-  pull(avg_len)
-
-even_odd_sd_len <- even_odd_stats %>%
-  pull(sd_len)
-
+# Printing Q3
+print(even_odd_stats)
 
 
 
