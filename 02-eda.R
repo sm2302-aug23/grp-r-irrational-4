fc8632292b3d41bdc65f468cddb6652d7383c6c9
azyansafriudin <22b6038@ubd.edu.bn>
2023-09-21 16:24:12 +0800

    update 02

--- 02-eda.R
+++ 02-eda.R
@@ -1,7 +1,6 @@
-library(tibble)
-library(dplyr)
 
-# Create a data frame with integers from 1 to 10,000
+
+# Create a tibble with integers from 1 to 10,000 and generate Collatz sequences
 collatz_df <- tibble(start = 1:10000) %>%
   mutate(seq = map(start, gen_collatz))
 
@@ -13,15 +12,17 @@ collatz_df <- collatz_df %>%
     max_val = map_dbl(seq, max)
   )
 
+# Question 1: Find the top 10 starting integers that produce the longest sequences [top10longest].
 top10longest <- collatz_df %>%
   arrange(desc(length)) %>%
   slice(1:10)
 
-max_Val_int <- collatz_df %>%
+# Question 2: Find out which starting integer produces a sequence that reaches the highest maximum value [max_val_int].
+max_val_int <- collatz_df %>%
   filter(max_val == max(max_val)) %>%
   select(start)
 
-
+# Question 3: Calculate the average length and standard deviation of the sequence for even and odd starting integers.
 even_odd_stats <- collatz_df %>%
   group_by(parity) %>%
   summarize(
@@ -29,9 +30,9 @@ even_odd_stats <- collatz_df %>%
     even_odd_sd_len = sd(length)
   )
 
+# Extract the results
 even_odd_avg_len <- even_odd_stats$even_odd_avg_len
 even_odd_sd_len <- even_odd_stats$even_odd_sd_len
-
 
 
 
