f3ed3d56623cabb0a83346fb2366761990aaf8fc
azyansafriudin <22b6038@ubd.edu.bn>
2023-09-25 23:36:54 +0800

    Update 06 as of 259

--- 06-creative.R
+++ 06-creative.R
@@ -1,6 +1,30 @@
 # Task 6
 
-library(tidyverse)
+# Define a function to calculate Collatz sequence length
+collatz_length <- function(n) {
+  count <- 0
+  while (n != 1) {
+    if (n %% 2 == 0) {
+      n <- n / 2
+    } else {
+      n <- 3 * n + 1
+    }
+    count <- count + 1
+  }
+  return(count)
+}
+
+# Create a vector of starting integers
+start_integers <- 1:100
+
+# Calculate sequence lengths for the starting integers
+sequence_lengths <- sapply(start_integers, collatz_length)
+
+# Create a simple line plot to visualize the change in sequence lengths
+plot(start_integers, sequence_lengths, type = "l", 
+     xlab = "Starting Integer", ylab = "Sequence Length",
+     main = "Change in Collatz Sequence Lengths",
+     col = "blue")
 
 
 
